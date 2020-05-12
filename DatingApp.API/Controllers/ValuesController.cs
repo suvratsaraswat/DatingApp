810c626fdf03ca5eafca154954fca49384745b31
suvratsaraswat <59220700+suvratsaraswat@users.noreply.github.com>
2020-05-12 14:04:54 +0530

    End of Section 4

--- DatingApp.API/Controllers/ValuesController.cs
+++ DatingApp.API/Controllers/ValuesController.cs
@@ -17,9 +17,10 @@ namespace DatingApp.API.Controllers
         private readonly DataContext _context;
         public ValuesController(DataContext context)
         {
-            this._context = context;
+            _context = context;
         }
-        // GET api/values
+        
+        [AllowAnonymous]
         [HttpGet]
         public async Task<IActionResult> GetValues()
         {
